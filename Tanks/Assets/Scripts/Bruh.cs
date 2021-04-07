a008219d725c73d91ade00ffb8aca66170e9759b
Taptrue1 <82027635+Taptrue1@users.noreply.github.com>
2021-04-07 19:48:43 +0300

    Update Bruh.cs
    Поменял пару строчек кода

--- Tanks/Assets/Scripts/Bruh.cs
+++ Tanks/Assets/Scripts/Bruh.cs
@@ -4,14 +4,16 @@ public class Bruh : MonoBehaviour
 {
     public int Bruh1 = 0;
     public long Bruh2 = 0;
-	private bool Bruh3PoshelTaskatJashchiki = true;
+	private bool _bruh3PoshelTaskatJashchiki = true;
 
     private void Update()
     {
         Debug.Log("Bruh");
 		if (Bruh3PoshelTaskatJashchiki == true) 
-			Debug.Log("Vito Scaletta любит таскать ящики, 
-			получать шиш и ломать грузовик.");
+			Debug.Log("Vito Scaletta любит таскать ящики, получать шиш и ломать грузовик.");
+		
+		if(Bruh1 > Bruh2)
+			Debug.Log("Bruh сломался");
     }
 	//
 	// Линк, любишь же ты заставлять таскать
