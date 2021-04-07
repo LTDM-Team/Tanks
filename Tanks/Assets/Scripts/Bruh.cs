5f5f4cab3a61576df41625134a5a5d0a39308838
JustLink <linki_mail@mail.ru>
2021-04-07 18:22:46 +0700

    Исправлена инициализация bruh-ов

--- Tanks/Assets/Scripts/Bruh.cs
+++ Tanks/Assets/Scripts/Bruh.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class Bruh : MonoBehaviour
 {
-    public int Bruh1 = (int)1 ^ (int)(long)(int).123456789 >> (int)20;
-    public long Bruh2 = (long)1 ^ (long)(int)(long).123456789 >> (int)20;
+    public int Bruh1 = 0;
+    public long Bruh2 = 0;
 
     private void Update()
     {
