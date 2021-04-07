dd49b6ed53731f3aadd018053b872323eb55ff65
Oleksii <51969410+djighoul29@users.noreply.github.com>
2021-04-07 19:12:44 +0300

    Update Bruh.cs
    ABOBA

--- Tanks/Assets/Scripts/Bruh.cs
+++ Tanks/Assets/Scripts/Bruh.cs
@@ -4,9 +4,11 @@ public class Bruh : MonoBehaviour
 {
     public int Bruh1 = 0;
     public long Bruh2 = 0;
+	private bool Bruh3_poshel_nosit_jashchiki = true;
 
     private void Update()
     {
         Debug.Log("Bruh");
+		if (Bruh3_poshel_nosit_jashchiki == true) Debug.Log("Vito Scaletta любит таскать ящики, получать шиш и ломать грузовик.");
     }
 }
