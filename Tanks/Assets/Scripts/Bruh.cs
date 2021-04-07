3af38ccbbb3569fcca47908e16e5440ba460f74b
Oleksii <51969410+djighoul29@users.noreply.github.com>
2021-04-07 19:28:20 +0300

    Update Bruh.cs
    Link made me to do another one commit

--- Tanks/Assets/Scripts/Bruh.cs
+++ Tanks/Assets/Scripts/Bruh.cs
@@ -4,11 +4,18 @@ public class Bruh : MonoBehaviour
 {
     public int Bruh1 = 0;
     public long Bruh2 = 0;
-	private bool Bruh3_poshel_nosit_jashchiki = true;
+	private bool Bruh3PoshelTaskatJashchiki = true;
 
     private void Update()
     {
         Debug.Log("Bruh");
-		if (Bruh3_poshel_nosit_jashchiki == true) Debug.Log("Vito Scaletta любит таскать ящики, получать шиш и ломать грузовик.");
+		if (Bruh3PoshelTaskatJashchiki == true) 
+			Debug.Log("Vito Scaletta любит таскать ящики, 
+			получать шиш и ломать грузовик.");
     }
+	//
+	// Линк, любишь же ты заставлять таскать
+	// ящики и возиться целый день :)
+	// Приколы из видосов Deadyson, кому интересно
+	//	
 }
