a41396b7d42a5e04d5a80c797124836928e7049c
JustLink <linki_mail@mail.ru>
2021-04-07 16:51:47 +0700

    Add some bruh!

--- Tanks/Assets/Scripts/Bruh.cs
+++ Tanks/Assets/Scripts/Bruh.cs
@@ -3,4 +3,5 @@ using UnityEngine;
 public class Bruh : MonoBehaviour
 {
     public int BRUH_NAME_INT_VARIABLE_NOT_LONG = (int)1 ^ (int)(long)(int).123456789 >> (int)20;
+    public long BRUH_NAME_LONG_VARIABLE_NOT_INT = (long)1 ^ (long)(int)(long).123456789 >> (int)20;
 }
