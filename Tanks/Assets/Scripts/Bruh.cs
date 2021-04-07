13af96ad61a66c404d79f056df886d74a5103acc
JustLink <linki_mail@mail.ru>
2021-04-07 16:57:25 +0700

    Fix naming?

--- Tanks/Assets/Scripts/Bruh.cs
+++ Tanks/Assets/Scripts/Bruh.cs
@@ -2,6 +2,6 @@ using UnityEngine;
 
 public class Bruh : MonoBehaviour
 {
-    public int BRUH_NAME_INT_VARIABLE_NOT_LONG = (int)1 ^ (int)(long)(int).123456789 >> (int)20;
-    public long BRUH_NAME_LONG_VARIABLE_NOT_INT = (long)1 ^ (long)(int)(long).123456789 >> (int)20;
+    public int Bruh1 = (int)1 ^ (int)(long)(int).123456789 >> (int)20;
+    public long Bruh2 = (long)1 ^ (long)(int)(long).123456789 >> (int)20;
 }
