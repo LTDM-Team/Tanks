10f098d06437571520b0de6e29f445dc4a035806
JustLink <linki_mail@mail.ru>
2021-04-20 00:02:56 +0700

    Added papa carla

--- Tanks/Assets/Scripts/NewTestScript.cs
+++ Tanks/Assets/Scripts/NewTestScript.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
 
-public class NewTestScript : MonoBehaviour
-{
+public class NewTestScript : MonoBehaviour {
+    public int _pa_pa_carla = 1;
+
     public void Update()
     {
         if (true)
             Debug.Log("DirtyCall");
         else Debug.Log("DirtyCall");
 
-        Debug.Log("Допустим");
-    }
+         Debug.Log("papa");
+
+
+
+
+         float ABOBA = 0f;
+          _pa_pa_carla = _pa_pa_carla - _pa_pa_carla + _pa_pa_carla;
+     }
 }
