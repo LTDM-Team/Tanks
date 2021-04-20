914f3c5c866aed4129ca13882bbeab05c9139016
JustLink <linki_mail@mail.ru>
2021-04-20 10:31:11 +0700

    Update NewTestScript.cs

--- Tanks/Assets/Scripts/NewTestScript.cs
+++ Tanks/Assets/Scripts/NewTestScript.cs
@@ -10,12 +10,30 @@ public class NewTestScript : MonoBehaviour
             Debug.Log("DirtyCall");
         else Debug.Log("DirtyCall");
 
-         Debug.Log("papa");
+        Debug.Log("papa");
 
-         float aboba = 0f;
-          _paPaCarla = _paPaCarla - _paPaCarla + _paPaCarla;
+        float aboba = 0f;
+        _paPaCarla = _paPaCarla - _paPaCarla + _paPaCarla;
 
-                                      Debug.Log("Abobnik");
-                                                                   var pipka = 0 < 3;
-     }
+        Debug.Log("Abobnik");
+        var pipka = 0 < 3;
+
+        Debug.Log("Abobnik");
+        Debug.Log("Abobnik");
+        Debug.Log("Abobnik");
+
+        _paPaCarla = _paPaCarla - _paPaCarla + _paPaCarla;
+        Debug.Log("Abobnik");
+
+        _paPaCarla = _paPaCarla - _paPaCarla + _paPaCarla;
+        Debug.Log("Abobnik");
+
+        if (true)
+            Debug.Log("DirtyCall");
+        else Debug.Log("DirtyCall");
+
+        if (true)
+            Debug.Log("DirtyCall");
+        else Debug.Log("DirtyCall");
+    }
 }
